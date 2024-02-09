b3fdba9b8871241a49d1a35ada32844c9d7a2a1c
Empire688682 <asehindej@gmail.com>
2024-02-09 14:46:27 +0100

    first Action

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-         <div class="container">
-            <p class="designer"> &copy Copyrighted 2023 <a href="#J">JAY-EMPIRE</a> <i class="fa fa-heart"></i> All rights reserved </p>
-         </div>
-</footer>
